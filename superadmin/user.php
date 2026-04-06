f784760cc7e3e9e39d01855a16f781dc588ca19b
ankitchauhan1221 <ankitchauhan1221@gmail.com>
2026-04-06 12:40:22 +0530

    kjhuijhuiokui

---
+++ superadmin/user.php
@@ -0,0 +1,73 @@
+
+<?php include 'header.php'; ?>
+            <!-- ========== Topbar End ========== -->
+            <div class="content">
+
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="d-flex justify-content-between align-items-center py-2">
+                                <h4>User</h4>
+
+
+                                <ol class="breadcrumb d-lg-flex d-none mb-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">TechUI</a></li>
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Ecommerce</a></li>
+
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">User</a></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                   
+                                </div>
+
+                                <div class="table-responsive">
+                                    <table class="table table-centered w-100 nowrap mb-0">
+                                        <thead class="table-light">
+                                            <tr>
+                                                <th>
+                                                  S.No
+                                                </th>
+                                               
+                                                <th> Name</th>
+                                               <th>Email</th>
+                                             
+                                                
+                                              
+                                                <th>Mobile</th>
+                                                <th>Create Date</th>
+                                             
+                                                
+                                                <th>Status</th>
+                                         
+                                             
+                                            
+                                            </tr>
+                                        </thead>
+                                        <tbody id="alluser">
+                                      
+
+                                         
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+                    </div>
+                    <!-- end row -->
+
+                </div> <!-- container -->
+
+            </div> <!-- content -->
+
+            <?php include 'footer.php'; ?>
